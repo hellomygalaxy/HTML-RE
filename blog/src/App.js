17be5d5c149cf2925e5d6f73470c5b8c626a664e
DIANNA CADEN <44316764+hellomygalaxy@users.noreply.github.com>
2022-06-14 09:10:48 +0900

    react basic 3

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  let [글제목, 글제목변경함수] = useState(["블랙 말랑 카우", "핑크 말랑 카우"]);
+  let [따봉, 따봉변경] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black_cow">
+        <div className="dogfoot">개발 TEST</div>
+      </div>
+      <div className="list">
+        <h3>
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {따봉}
+        </h3>
+        <p>2월 18일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {글제목[1]}</h3>
+        <p>2월 18일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
